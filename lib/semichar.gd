2a6f50d14122e34f1379cdf0e9352165c3f2d055
Pete Gautam <pgdchs@hotmail.co.uk>
2026-07-22 12:47:05 +0100

    Add semicharacharacteristic functions

---
+++ lib/semichar.gd
@@ -0,0 +1,7 @@
+DeclareAttribute("SemiAutomorphismGroup", IsPGroup);
+
+DeclareOperation("IsSemicharacteristicSubgroup", [IsPGroup, IsPGroup]);
+
+DeclareAttribute("SemicharacteristicSubgroups", IsPGroup);
+
+DeclareOperation("SemicharacteristicClosure", [IsPGroup, IsPGroup]);
